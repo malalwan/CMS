bb027a714a5eab21569b2fbf9bc605094ef7189d
malalwan <51250170+malalwan@users.noreply.github.com>
2019-06-10 18:30:20 +0530

    Stage 1 added

---
+++ globe_bank/private/shared/staff_header.php
@@ -0,0 +1,20 @@
+<?php if(!isset($page_title)) { $page_title = 'Staff Area'; } ?>
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>GBI - <?= $page_title; ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+  </head>
+
+  <body>
+    <header>
+      <h1>GBI Staff Area</h1>
+    </header>
+    
+    <nav>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </nav>
